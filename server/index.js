50c2a080abc605d8eabad8f36606f3647dfe3e31
Ster <sterz.dev@gmail.com>
2021-09-25 11:12:25 -0400

    Update project structure

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/users", require("./routes/user.routes.js"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
