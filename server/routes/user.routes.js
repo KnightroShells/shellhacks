ac40fbe7aad3ffa79ad01385be8f52ed7076799a
Ster <sterz.dev@gmail.com>
2021-09-25 20:57:18 -0400

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -43,37 +43,7 @@ router.post("/register", async (req, res) => {
 
 // Login
 router.post("/login", async (req, res) => {
-  const {email, password} = req.body;
-
-  if(!email || !password)
-    return res.status(400).json({error: "Empty field in form input."})
-
-  if(email.length < 5 || email.length > 255)
-    return res.status(400).json({error: "Invalid email address."})
-
-  if(password.length < 6 || email.length > 40)
-    return res.status(400).json({error: "Password must be between 6 and 40 characters."})
-
-  let exists = await User.findOne({ where: { email: email }})
-  if(exists){
-    res.status(409).json({error: "An account with this email already exists."}); // 409 = conflict error code
-    return;
-  }
-
-  bcrypt.hash(password, 10)
-    .then(async (hash) => {
-      let user = await User.create({
-        email: email, 
-        password: hash,
-        interests: "",
-      });
-    })
-    .catch(error => {
-      res.status(500).json({error: error});
-    });
-
-
-  res.status(201).send();
+  // TODO with JWT
 });
 
 router.put("/updateInterests", async (req, res) => {
